cf41807b00d787fed0ba7dc4dce92a5c830e0672
KevinFairuz <kevin.10122346@mahasiswa.unikom.ac.id>
2023-10-27 18:41:45 +0700

    update

--- repositories/users.js
+++ repositories/users.js
@@ -1,37 +1,44 @@
-import dbPool from "../utils/db.js";
+import dbPool from '../utils/db.js';
 
 const getData = () => {
-    const query = "SELECT user_id, name, email, password, created_at FROM users";
-    
-    return dbPool.query(query);
-}
+  const query = 'SELECT user_id, name, email, password, created_at FROM users';
+
+  return dbPool.query(query);
+};
 
 const createData = (name, email, password) => {
-    let createdAt = new Date();
+  let createdAt = new Date();
 
-    const query = "INSERT INTO users(name, email, password, created_at) VALUES (?, ?, ?, ?)";
-    const values = [name, email, password, createdAt];
+  const query =
+    'INSERT INTO users(name, email, password, created_at) VALUES (?, ?, ?, ?)';
+  const values = [name, email, password, createdAt];
 
-    return dbPool.query(query,values);
-}
+  return dbPool.query(query, values);
+};
 
 const getDataById = (id) => {
-    const query = "SELECT user_id, name, email, password, created_at FROM users WHERE user_id = ?";
-    
-    return dbPool.query(query, [id]);
-}
+  const query =
+    'SELECT user_id, name, email, password, created_at FROM users WHERE user_id = ?';
+
+  return dbPool.query(query, [id]);
+};
 
 const deleteData = (id) => {
-    const query = "DELETE FROM users WHERE user_id = ?";
-    
-    return dbPool.query(query, [id]);
-}
+  const query = 'DELETE FROM users WHERE user_id = ?';
 
-const updateData = (id) => {
-    const query = "UPDATE FROM users WHERE user_id = ?";
-    
-    return dbPool.query(query, [id]);
-}
+  return dbPool.query(query, [id]);
+};
 
+const updateData = (id, updatedData) => {
+  const query =
+    'UPDATE users SET name = ?, email = ?, password = ? WHERE user_id = ?';
 
-export { getData, createData, getDataById, deleteData, updateData }
+  return dbPool.query(query, [
+    updatedData.name,
+    updatedData.email,
+    updatedData.password,
+    id,
+  ]);
+};
+
+export { getData, createData, getDataById, deleteData, updateData };
